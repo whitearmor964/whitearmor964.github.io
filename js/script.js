4d8ffc675b6c5bc3fb23f0f0b3e7521038734820
Alex T <77662797+whitearmor964@users.noreply.github.com>
2021-01-30 13:46:10 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $('.header_burger').click(function(event){
-        $('.header_burger, .header_menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
